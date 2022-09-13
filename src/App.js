f697c3f701d2eedb5e1235c95fdfa87c63b5fd05
SebastianG9912 <sebastian.gustafsson@utb.ecutbildning.se>
2022-09-13 13:44:26 +0200

    "Home"-view styling done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import "./App.css";
+import "./components/styles.css";
+import { Link } from "react-router-dom";
 import { Links } from "./components";
 
 function App() {
   return (
     <div>
       <Links />
-      This is the startpage
+      <div style={{ margin: "10vh" }}>
+        <h1 style={{ textAlign: "center" }}>Welcome!</h1>
+        <div style={{ margin: "auto", maxWidth: "500px" }}>
+          <p style={{ lineHeight: "1.5em" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            luctus risus et metus vestibulum, in suscipit lorem venenatis.
+            Quisque faucibus aliquet laoreet. Cras varius libero in faucibus
+            maximus. Nullam vitae velit vel orci tincidunt bibendum non eget
+            erat.
+          </p>
+        </div>
+
+        <div
+          style={{
+            width: "fit-content",
+            display: "flex",
+            margin: "auto",
+          }}
+        >
+          <Link class="btn-secondary" to="/Register">
+            Register for interest
+          </Link>
+          <Link class="btn-secondary" to="/Newsfeed">
+            See the newsfeed
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 const Links = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/Register">Register for interest</Link>
-            <Link to="/Newsfeed">See the newsfeed</Link>  
-        </nav>
+        <header style={{paddingLeft:"30px"}}>
+            <nav class="links">
+                <Link to="/">Home</Link>
+                <Link to="/Register">Register email</Link>
+                <Link to="/Newsfeed">Newsfeed</Link>  
+            </nav>
+        </header>
     );
 }
 
